f529c5d743a452087cb0067f0f662036af354638
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2016-09-09 14:20:44 -0500

    refactor(config): move map api key

---
+++ app/config.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.config = {
+    "GOOGLE_MAP": 'AIzaSyBpgAKFAdX_Lwpshvg_KhbIDNVJb1xivHo'
+};
+//# sourceMappingURL=config.js.map
---
+++ app/config.ts
@@ -0,0 +1,3 @@
+export const config: any = {
+    "GOOGLE_MAP": 'AIzaSyBpgAKFAdX_Lwpshvg_KhbIDNVJb1xivHo'
+};
