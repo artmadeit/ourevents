3f2d4e654ab160119bdfe30d4eb4a305bfdec2c6
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2016-09-14 13:54:13 -0500

    chore(app): remove js and map files

--- app/components/auth/users.mock.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.MOCK_USERS = [
-    { name: 'arthur', email: 'this.arthur.email@gmail.com', password: 'root' },
-    { name: 'diana', email: 'qpdian@gmail.com', password: 'qpdian' },
-];
-//# sourceMappingURL=users.mock.js.map
