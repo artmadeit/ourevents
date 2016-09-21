1b1520c21090736dadbd3481e3512c73ac311e06
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2016-09-21 14:52:44 -0500

    chore(auth-register): move to reactive forms

--- app/components/auth/register/register.component.ts
+++ app/components/auth/register/register.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../user';
 import { RegisterUserService } from './register.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// FIX this in ng2-custom-validation..to use ng2-custom-validation/core 
+// TODO make typed message bag to message bag..
+import { ValidationMessagesService, MessageBag } from 'ng2-custom-validation/src/index';
 
 @Component({
     moduleId: module.id,
@@ -23,23 +26,54 @@ import { Router } from '@angular/router';
     `]
 })
 export class RegisterComponent implements OnInit {
-    user: User;
+    errorMessage: string;
+    userForm: FormGroup;
+    errors: MessageBag;
 
     constructor(
         private registerUser: RegisterUserService,
-        private router: Router) {
-        this.user = new User('', '', '');
+        private router: Router,
+        private formBuilder: FormBuilder,
+        private validationMessagesService: ValidationMessagesService) {
+        this.buildForm();
     }
 
     ngOnInit() { }
 
-    register() {
-        // TODO: only register if is valid
-        this.registerUser.execute(this.user);
-        this.router.navigate(['']);
+    private buildForm() {
+        this.userForm = this.formBuilder.group(
+            {
+                'name': ['', [
+                    Validators.required,
+                    Validators.maxLength(50)
+                ]],
+                'email': ['', [
+                    Validators.required,
+                    Validators.maxLength(50)
+                ]],
+                'password': ['', [
+                    Validators.required,
+                    Validators.minLength(6),
+                    Validators.maxLength(50)
+                ]]
+            });
+
+        this.userForm.valueChanges
+            .subscribe(data => {
+                this.seeForErrors();
+            });
+        this.seeForErrors(); // (re)set validation messages now
     }
 
-    get isValid(): boolean{
-        return true;
+    private seeForErrors() {
+        this.validationMessagesService
+            .build(this.userForm)
+            .subscribe((errors: MessageBag) => this.errors = errors);
+    }
+
+    register() {
+        // FIXME: only register if is valid
+        this.registerUser.execute(this.userForm.value);
+        this.router.navigate(['']);
     }
 }
