5b30e826afdd220df2407b836dd19d18405bbc33
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2016-09-22 15:24:31 -0500

    refactor(shared.module):  removed Forms

--- app/shared.module.ts
+++ app/shared.module.ts
@@ -3,9 +3,7 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-// TODO remove FormsModule..use ReactiveFormModule instead
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // FIX this in ng2-custom-validation..to use ng2-custom-validation/core 
 import { ValidationMessagesModule } from 'ng2-custom-validation/src/index';
@@ -18,19 +16,16 @@ import { MdToolbarModule } from '@angular2-material/toolbar';
 import { MdListModule } from '@angular2-material/list';
 import { MdInputModule } from '@angular2-material/input';
 import { MdMenuModule } from '@angular2-material/menu';
-import { MdGridListModule } from '@angular2-material/grid-list';
 import { MdRadioModule } from '@angular2-material/radio';
 
 @NgModule({
     imports: [
         MdMenuModule.forRoot(),
         ValidationMessagesModule.forRoot(),
-        MdGridListModule.forRoot(),
         MdRadioModule.forRoot()
     ],
     exports: [
         CommonModule,
-        FormsModule,
         ReactiveFormsModule,
         MdButtonModule,
         MdCardModule,
@@ -40,7 +35,6 @@ import { MdRadioModule } from '@angular2-material/radio';
         MdListModule,
         MdInputModule,
         MdMenuModule,
-        MdGridListModule,
         MdRadioModule
     ],
     declarations: [],
