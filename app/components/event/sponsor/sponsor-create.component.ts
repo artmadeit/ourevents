c19eb121037ffab64262bbafa49fd72dac6ca487
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2016-09-29 00:44:07 -0500

    feat(sponsor-create): add form reactive

--- app/components/event/sponsor/sponsor-create.component.ts
+++ app/components/event/sponsor/sponsor-create.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Event, EventRole, EventRoleTypes } from '../index';
 import { Party } from '../../party/party';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 @Component({
     moduleId: module.id,
     selector: 'sponsor-create',
@@ -10,19 +12,30 @@ import { Party } from '../../party/party';
 export class SponsorCreateComponent implements OnInit {
     @Input()
     event: Event;
+    form: FormGroup;
     private selectedOrganization: any;
 
-    constructor() { }
+    constructor(private formBuilder: FormBuilder) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.buildForm();
+    }
 
-    addSponsorType(sponsorType: any) {
-        EventRole.create(this.selectedOrganization, this.event, EventRoleTypes.Sponsor, sponsorType.value);
-        sponsorType.value = '';
+    private buildForm() {
+        this.form = this.formBuilder.group(
+            {
+                'name': ['', [Validators.required]],
+                'type': ['', [Validators.required]]
+            });
     }
 
     getSelectedSponsor(selectedSponsor: Party) {
         this.selectedOrganization = selectedSponsor;
+    }
+
+    addSponsor() {
+        EventRole.create(this.selectedOrganization, this.event, EventRoleTypes.Sponsor, this.form.value['type']);
+        this.form.reset();
     }
 
 }
